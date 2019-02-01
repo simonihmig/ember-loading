11ffbb05cd9df3872ae57a591815c464ec46d2ea
simonihmig <simon.ihmig@kaliber5.de>
2019-02-01 22:41:18 +0100

    Make while-loading tagless

--- addon/components/while-loading.ts
+++ addon/components/while-loading.ts
@@ -3,7 +3,9 @@ import Component from '@ember/component';
 import layout from '../templates/components/while-loading';
 import { inject as service } from '@ember-decorators/service';
 import LoadingService from '../services/loading';
+import { tagName } from '@ember-decorators/component';
 
+@tagName('')
 export default class WhileLoading extends Component{
   layout = layout;
 
