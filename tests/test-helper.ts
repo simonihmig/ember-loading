dc1706c820ce4d8d4d561915b54c8931429c731f
simonihmig <simon.ihmig@kaliber5.de>
2021-03-04 15:56:52 +0100

    Fix TS for precompilation

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,5 +1,6 @@
-import Application from 'dummy/app';
-import config from 'dummy/config/environment';
+// @ts-ignore
+import Application from '../app';
+import config from '../config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
