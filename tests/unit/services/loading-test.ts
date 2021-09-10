4257a3776667b47380ffb2b402ba599ac9ebb943
simonihmig <simon.ihmig@kaliber5.de>
2021-09-10 11:51:04 +0200

    Better test assertion messages, stabilize flaky tests

--- tests/unit/services/loading-test.ts
+++ tests/unit/services/loading-test.ts
@@ -20,18 +20,18 @@ module('Unit | Service | loading', function(hooks) {
       };
 
       let service: LoadingService = this.owner.lookup('service:loading');
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading before run has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state before run has started');
 
       let promise = service.run(job);
       assert.equal(called, 1, 'job has been called');
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after run has started');
+      assert.ok(service.showLoading, 'it is showing loading state after run has started');
 
       deferred.resolve();
       await promise;
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after run has finished');
+      assert.notOk(service.showLoading, 'it is not showing loading state after run has finished');
     });
 
     test('waits for all async jobs', async function(assert) {
@@ -39,26 +39,26 @@ module('Unit | Service | loading', function(hooks) {
       let deferred2 = defer();
 
       let service: LoadingService = this.owner.lookup('service:loading');
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading before run has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state before run has started');
 
       let promise1 = service.run(() => deferred1.promise);
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after first run has started');
+      assert.ok(service.showLoading, 'it is showing loading state after first run has started');
 
       let promise2 = service.run(() => deferred2.promise);
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after second run has started');
+      assert.ok(service.showLoading, 'it is showing loading state after second run has started');
 
       deferred1.resolve();
       await promise1;
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is still loading after first run has finished');
+      assert.ok(service.showLoading, 'it is showing loading state after first run has finished');
 
       deferred2.resolve();
       await promise2;
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after second run has finished');
+      assert.notOk(service.showLoading, 'it is not showing loading state after second run has finished');
     });
 
     test('passes args', async function(assert) {
@@ -144,25 +144,25 @@ module('Unit | Service | loading', function(hooks) {
       let service: LoadingService = Service.create({
         postDelay: 10
       });
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading before run has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state before run has started');
 
       let promise = service.run(job);
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after run has started');
+      assert.ok(service.showLoading, 'it is showing loading state after run has started');
 
       deferred.resolve();
       await promise;
-      assert.notOk(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after run has finished');
+      assert.ok(service.showLoading, 'it is showing loading state after run has finished');
 
-      await timeout(6);
-      assert.notOk(service.isLoading);
-      assert.ok(service.showLoading);
+      await timeout(2);
+      assert.notOk(service.isLoading, 'it is not loading after run has finished, but postDelay has not finished');
+      assert.ok(service.showLoading, 'it is showing loading state after run has finished, but postDelay has not finished');
 
-      await timeout(5);
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      await timeout(10);
+      assert.notOk(service.isLoading, 'it is not loading after run has finished and postDelay has finished');
+      assert.notOk(service.showLoading, 'it is not showing loading state after run has finished and postDelay has finished');
     });
 
     test('supports preDelay', async function(assert) {
@@ -172,21 +172,21 @@ module('Unit | Service | loading', function(hooks) {
       let service: LoadingService = Service.create({
         preDelay: 10
       });
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading before run has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state before run has started');
 
       let promise = service.run(job);
-      assert.ok(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after run has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state after run has started but preDely has not finished');
 
-      await timeout(11);
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      await timeout(15);
+      assert.ok(service.isLoading, 'it is loading after run has started and preDelay has finished');
+      assert.ok(service.showLoading, 'it is showing loading state after run has started and preDelay has finished');
 
       deferred.resolve();
       await promise;
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after run has finished');
+      assert.notOk(service.showLoading, 'it is not showing loading state after run has finished');
     });
 
     test('reads config', async function(assert) {
@@ -220,41 +220,41 @@ module('Unit | Service | loading', function(hooks) {
       let service: LoadingService = this.owner.lookup('service:loading');
       let router = this.owner.lookup('service:router');
 
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading before routing change has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state before routing change has started');
 
       router.trigger('routeWillChange');
       await settled();
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after route change event');
+      assert.ok(service.showLoading, 'it is showing loading state after route change event');
 
       router.trigger('routeDidChange');
       await settled();
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after route has changed');
+      assert.notOk(service.showLoading, 'it is not showing loading state after route has changed');
     });
 
     test('multiple route transition events are handled correctly', async function(assert) {
       let service: LoadingService = this.owner.lookup('service:loading');
       let router = this.owner.lookup('service:router');
 
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading before routing change has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state before routing change has started');
 
       router.trigger('routeWillChange');
       await settled();
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after first route change event');
+      assert.ok(service.showLoading, 'it is showing loading state after first route change event');
 
       router.trigger('routeWillChange');
       await settled();
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after second route change event');
+      assert.ok(service.showLoading, 'it is showing loading state after second route change event');
 
       router.trigger('routeDidChange');
       await settled();
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after route has changed');
+      assert.notOk(service.showLoading, 'it is not showing loading state after route has changed');
     });
 
     test('supports postDelay', async function(assert) {
@@ -264,26 +264,26 @@ module('Unit | Service | loading', function(hooks) {
       });
       let router = this.owner.lookup('service:router');
 
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading before routing change has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state before routing change has started');
 
       router.trigger('routeWillChange');
       await timeout(0);
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after route change has started');
+      assert.ok(service.showLoading, 'it is showing loading state after route change has started');
 
       router.trigger('routeDidChange');
       await timeout(0);
-      assert.notOk(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after route has changed');
+      assert.ok(service.showLoading, 'it is still showing loading state after route has changed');
 
       await timeout(10);
-      assert.notOk(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after route has changed, but before postDelay has finished');
+      assert.ok(service.showLoading, 'it is still showing loading state after route has changed, but before postDelay has finished');
 
       await timeout(15);
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after route has changed and postDelay has finished');
+      assert.notOk(service.showLoading, 'it is not showing loading state after route has changed and postDelay has finished');
     });
 
     test('supports preDelay', async function(assert) {
@@ -293,22 +293,22 @@ module('Unit | Service | loading', function(hooks) {
       });
       let router = this.owner.lookup('service:router');
 
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading before routing change has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state before routing change has started');
 
       router.trigger('routeWillChange');
       await timeout(0);
-      assert.ok(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after route change has started');
+      assert.notOk(service.showLoading, 'it is not showing loading state unless preDelay has finished');
 
       await timeout(10);
-      assert.ok(service.isLoading);
-      assert.ok(service.showLoading);
+      assert.ok(service.isLoading, 'it is loading after preDelay has finished');
+      assert.ok(service.showLoading, 'it is showing loading state after preDelay has finished');
 
       router.trigger('routeDidChange');
       await timeout(0);
-      assert.notOk(service.isLoading);
-      assert.notOk(service.showLoading);
+      assert.notOk(service.isLoading, 'it is not loading after route has changed');
+      assert.notOk(service.showLoading, 'it is not showing loading state after route has changed');
     });
   });
 });
